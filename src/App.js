beb90991a0f29b3f26a7d4a7732aa18387b14f92
Maram Riahi <marameriahi03@gmail.com>
2024-04-20 10:02:15 +0100

    hello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Description from './Description/Description'
-//import Footer  from './components/Footer'
+import Footer  from './components/Footer'
 //import Slider from "./Carroussel/Slider";
 
 
